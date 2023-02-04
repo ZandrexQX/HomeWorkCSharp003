dda2e331d8f9dba3ed55aeafc29168c33870f40f
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-04 18:44:16 +0300

    Task 23

--- Program.cs
+++ Program.cs
@@ -16,34 +16,45 @@
 
 // Console.ReadLine();
 
+// /*--------------------------------------------*/
+
+// Console.Clear();
+// Console.WriteLine("Task 21");
+// Console.WriteLine("------");
+
+// Console.Write("Enter X1:");
+// double x1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Enter Y1:");
+// double y1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Enter Z1:");
+// double z1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Enter X2:");
+// double x2 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Enter Y2:");
+// double y2 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Enter Z2:");
+// double z2 = Convert.ToDouble(Console.ReadLine());
+
+// double length = Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2)+Math.Pow(z1-z2, 2));
+
+// Console.WriteLine($"Length: {Math.Round(length,2)}");
+
+// Console.ReadLine();
+
 /*--------------------------------------------*/
 
 Console.Clear();
-Console.WriteLine("Task 21");
+Console.WriteLine("Task 23");
 Console.WriteLine("------");
 
-Console.Write("Enter X1:");
-double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter Y1:");
-double y1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter Z1:");
-double z1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter X2:");
-double x2 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter Y2:");
-double y2 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter Z2:");
-double z2 = Convert.ToDouble(Console.ReadLine());
-
-double length = Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2)+Math.Pow(z1-z2, 2));
-
-Console.WriteLine($"Length: {Math.Round(length,2)}");
+for (int i = 1; i <= num2; i++) Console.Write($"{Math.Pow(i, 3)}  ");
 
 Console.ReadLine();
-
-/*--------------------------------------------*/
