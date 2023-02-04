3f1888739222dcc3edf507b9454f1a700f67064f
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-04 18:30:45 +0300

    Task 19

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Task 19");
+Console.WriteLine("------");
+
+Console.Write("Enter number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string str1 = num1.ToString();
+bool flag = true;
+for (int i = 0; i < str1.Length; i++){
+    if (str1[i] != str1[str1.Length-1-i]) flag = false;
+}
+
+if(flag) Console.WriteLine($"Number {num1} palindrome");
+else
+    Console.WriteLine($"Number {num1} not palindrome");
+
+Console.ReadLine();
+
+/*--------------------------------------------*/
